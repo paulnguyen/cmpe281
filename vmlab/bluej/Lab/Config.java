6ae9d00d6116b391ec42204dcefefbbc615635af
Paul Nguyen <paul.nguyen@sjsu.edu>
2012-09-04 18:56:18 -0700

    update with masked pwd

--- vmlab/bluej/Lab/Config.java
+++ vmlab/bluej/Lab/Config.java
@@ -4,6 +4,6 @@ public class Config
 {
     public static String getVmwareHostURL() { return "https://cmpe-admin-console.engr.sjsu.edu/sdk" ; }
     public static String getVmwareLogin() { return "student" ; }
-    public static String getVmwarePassword() { return "cmpe281@2012" ; }
+    public static String getVmwarePassword() { return "XXX" ; }
     public static String getVmwareVM() { return "Ubuntu Desktop" ; }
 }
