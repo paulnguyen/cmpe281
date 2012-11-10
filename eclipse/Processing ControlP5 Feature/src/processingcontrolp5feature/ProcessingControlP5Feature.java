4e675af9c2c1183d757c3f5a58ad9246253914a9
Paul Nguyen <paul.nguyen@sjsu.edu>
2012-11-10 10:51:34 -0800

    Initial Template for Processing AOP Experiments

---
+++ eclipse/Processing ControlP5 Feature/src/processingcontrolp5feature/ProcessingControlP5Feature.java
@@ -0,0 +1,17 @@
+package processingcontrolp5feature;
+
+import processing.core.PApplet;
+
+
+public class ProcessingControlP5Feature extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { processingcontrolp5feature.ProcessingControlP5Feature.class.getName() });
+	}
+}
