ceccebbab112f605f4503c76a4bcc2b5a9080c57
Paul Nguyen <paul.nguyen@sjsu.edu>
2013-01-27 22:29:01 -0800

    update

--- vmlab/bluej/Lab3/Config.java
+++
@@ -1,9 +0,0 @@
-package Lab2;
-
-public class Config
-{
-    public static String getVmwareHostURL() { return "https://cmpe-admin-console.engr.sjsu.edu/sdk" ; }
-    public static String getVmwareLogin() { return "XXX" ; }
-    public static String getVmwarePassword() { return "XXX" ; }
-    public static String getVmwareVM() { return "XXX" ; }
-}
