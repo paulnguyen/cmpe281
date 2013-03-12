414f9c8a31d04d7ad0b5e92dec4d73e9a95a3fa0
Paul Nguyen <paul.nguyen@sjsu.edu>
2013-03-11 23:57:17 -0700

    updated webservice example with sample code for client PUT, POST and DELETE

--- appfog/pnguyen-grails-webservices-v1/grails-app/controllers/pnguyen/grails/webservices/ApiClientController.groovy
+++ appfog/pnguyen-grails-webservices-v1/grails-app/controllers/pnguyen/grails/webservices/ApiClientController.groovy
@@ -6,10 +6,9 @@ import static groovyx.net.http.ContentType.XML
 class ApiClientController {
 
 	def index() {
-
-		def http = new HTTPBuilder("http://pnguyen-grails-webservices.aws.af.cm/api/show")
-		String output = "<hr/>"
-		
+		//def http = new HTTPBuilder("http://pnguyen-grails-webservices.aws.af.cm/api")
+		def http = new HTTPBuilder("http://localhost:8080/pnguyen-grails-webservices/api")
+		String output = "<hr/>"		
 		http.request(Method.GET, XML) 
 		{ 			
 			response.success = 
@@ -36,11 +35,74 @@ class ApiClientController {
 				}
 			} 
 		}
-		
 		output += "<hr/>"
 		render output
 	}
+	
+	
+	def postTest() {
+		//def http = new HTTPBuilder("http://pnguyen-grails-webservices.aws.af.cm/api")
+		def http = new HTTPBuilder("http://localhost:8080/pnguyen-grails-webservices/api")
+		String output = "<hr/>"
+		http.request(Method.POST, XML)
+		{
+			body =  """
+			<product id="3">
+			<name>MacBook Pro 17</name>
+			<price>2500.0</price>
+			<sku>PPPL123456789</sku>
+			<vendor id="1"/>
+			</product> 
+			""" ;			
+			response.success =
+			{ 	resp, xml ->
+				output = resp.status
+			}
+		}
+		output += "<hr/>"
+		render output
+	}
+	
+	
+	def putTest() {
+		//def http = new HTTPBuilder("http://pnguyen-grails-webservices.aws.af.cm/api/PPPL123456789")
+		def http = new HTTPBuilder("http://localhost:8080/pnguyen-grails-webservices/api/PPPL123456789")
+		String output = "<hr/>"
+		http.request(Method.PUT, XML)
+		{
+			body =  """
+			<product>
+					<price>3500.0</price>
+			</product>
+			""" ;			
+			response.success =
+			{ 	resp, xml ->
+				output = resp.status
+			}
+		}
+		output += "<hr/>"
+		render output
+	}
+
+	
+	def deleteTest() {
+		//def http = new HTTPBuilder("http://pnguyen-grails-webservices.aws.af.cm/api/PPPL123456789")
+		def http = new HTTPBuilder("http://localhost:8080/pnguyen-grails-webservices/api/PPPL123456789")
+		String output = "<hr/>"
+		http.request(Method.DELETE, XML)
+		{		
+			response.success =
+			{ 	resp, xml ->
+				output = resp.status
+			}
+		}
+		output += "<hr/>"
+		render output
+	}
+	
+	
 }
+
 
 
 /**
