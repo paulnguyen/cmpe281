b9215705d0b3585c3546889011dd903170cb11b3
Paul Nguyen <paul.nguyen@sjsu.edu>
2013-04-08 17:54:29 +0000

    update

---
+++ azure/vs2012/WindowsAzureGumball (V2)/WebRole1/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebRole1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        GumballMachineRepository db = new GumballMachineRepository();
+
+        String message = "";
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GumballMachine gm = db.getBySerialNumber("1234998871109");
+            message = "" +
+                       "Mighty Gumball, Inc.\n" +
+                       "Azure-enabled Standing Gumball\n" +
+                       "Model# " + gm.modelNumber + "\n" +
+                       "Serial# " + gm.serialNumber + "\n" +
+                       "Inventory: " + gm.count + " gumballs\n";
+            TextBoxMessage.Text = message;
+
+        }
+
+        protected void InsertQuaterButton_Click(object sender, EventArgs e)
+        {
+            TextBoxMessage.Text = message + "\n\nInsert Quarter Button Clicked!";
+        }
+
+        protected void TurnCrankButton_Click(object sender, EventArgs e)
+        {
+            TextBoxMessage.Text = message + "\n\nTurn Crank Button Clicked!";
+            GumballMachine gm = db.getBySerialNumber("1234998871109");
+            gm.count--;
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ azure/vs2012/WindowsAzureGumball (V2)/WebRole1/GumballDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1
+{
+    public class GumballDbContext : System.Data.Entity.DbContext
+    {
+        public DbSet<GumballMachine> gumballMachines { get; set; }
+    }
+}
