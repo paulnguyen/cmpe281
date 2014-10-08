76d04958a4bfe2bbb35becc8de770bc80c108a11
Paul Nguyen <paul.nguyen@sjsu.edu>
2014-10-08 10:07:55 -0700

    updated for Pivotal Web Services / Cloud Foundry Deployment with GGTS 3.6.1 / Grails 2.4.3

--- grails/GrailsGumballMachineVer1/grails-app/conf/DataSource.groovy
+++ grails/GrailsGumballMachineVer1/grails-app/conf/DataSource.groovy
@@ -1,57 +1,44 @@
 dataSource {
-    pooled = true
-    jmxExport = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+	pooled = true
+	jmxExport = true
+	driverClassName = "org.h2.Driver"
+	username = "sa"
+	password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
-    singleSession = true // configure OSIV singleSession mode
-    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+	cache.use_second_level_cache = true
+	cache.use_query_cache = false
+	//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+	cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+	singleSession = true // configure OSIV singleSession mode
+	flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
-            }
-        }
-    }
+	development {
+		dataSource {
+			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"  // 'create', 'create-drop','update'
+			url = "jdbc:mysql://localhost:3306/cmpe281"
+			driverClassName = "com.mysql.jdbc.Driver"
+			username = "root"
+			password = ""
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"  // 'create', 'create-drop','update'
+			url = "jdbc:mysql://b1a484021e4724:69ce939f@us-cdbr-iron-east-01.cleardb.net:3306/ad_5f54d5a266bf2a8"
+			driverClassName = "com.mysql.jdbc.Driver"
+			username = "b1a484021e4724"
+			password = "69ce939f"
+		}
+
+	}
 }
