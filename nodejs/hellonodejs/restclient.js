baf3a8d053c8fab9678811f4a8bd0df70ac30134
Paul Nguyen <paul.nguyen@sjsu.edu>
2014-11-16 23:47:13 -0800

    .

--- nodejs/hellonodejs/restclient.js
+++ nodejs/hellonodejs/restclient.js
@@ -3,9 +3,6 @@ var http = require('http') ;
 
 function request_handler (req, res) {
 
-    var body = "Hey, Thanks for calling!\n" ;
-    var content_length = body.length ;
-
     res.writeHead( 200, {
         'Content-Type' : "text/plain",
         'Content-Length' : content_length
