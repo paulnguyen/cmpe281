a21ace61e6e2ecb794637292be08d3dc6223757a
Paul Nguyen <paul.nguyen@sjsu.edu>
2015-03-05 17:12:40 -0800

    cleanup

--- grails/GrailsRestServiceVer1/grails-app/domain/grailsrestservicever1/Book.groovy
+++
@@ -1,9 +0,0 @@
-package grailsrestservicever1
-
-import grails.rest.*
-
-@Resource(uri='/books',formats=['json', 'xml']) 
-class Book {
-	String title
-	static constraints = { title blank:false } 
-}
