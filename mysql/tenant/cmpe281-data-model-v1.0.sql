2b5e874203545551370f825d70a5b2fb3a65a048
Paul Nguyen <paul.nguyen@sjsu.edu>
2017-03-09 18:52:45 -0800

    2017 Update

--- mysql/tenant/cmpe281-data-model-v1.0.sql
+++ mysql/tenant/cmpe281-data-model-v1.0.sql
@@ -1,8 +1,52 @@
 
+
+CREATE TABLE IF NOT EXISTS `cmpe281`.`LP_TASK` (
+  `ID` INT NOT NULL,
+  `DESC` VARCHAR(45) NULL,
+  `OWNER` VARCHAR(45) NULL,
+  `PLAN_START` DATETIME NULL,
+  `PLAN_FINISH` DATETIME NULL,
+  `DONE` VARCHAR(45) NULL)
+ENGINE = InnoDB;
+
+
+-- TENANT TABLE
+insert into TENANT_TABLE( TENANT_ID, TABLE_NAME, TABLE_DESC )
+values ( 'TA', 'LP_TASK', 'Task Table for LiquidPlanner' ) ;
+
+-- TENANT FIELDS
+insert into TENANT_FIELDS( TENANT_ID, TABLE_NAME, FIELD_NAME, FIELD_TYPE, FIELD_COLUMN ) values ( 'TA', 'LP_TASK', 'ID', 'INT', 1 ) ;
+insert into TENANT_FIELDS( TENANT_ID, TABLE_NAME, FIELD_NAME, FIELD_TYPE, FIELD_COLUMN ) values ( 'TA', 'LP_TASK', 'DESC', 'VARCHAR(45)', 2 ) ;
+insert into TENANT_FIELDS( TENANT_ID, TABLE_NAME, FIELD_NAME, FIELD_TYPE, FIELD_COLUMN ) values ( 'TA', 'LP_TASK', 'OWNER', 'VARCHAR(45)', 3 ) ;
+insert into TENANT_FIELDS( TENANT_ID, TABLE_NAME, FIELD_NAME, FIELD_TYPE, FIELD_COLUMN ) values ( 'TA', 'LP_TASK', 'PLAN_START', 'DATETIME', 4 ) ;
+insert into TENANT_FIELDS( TENANT_ID, TABLE_NAME, FIELD_NAME, FIELD_TYPE, FIELD_COLUMN ) values ( 'TA', 'LP_TASK', 'PLAN_FINISH', 'DATETIME', 5 ) ;
+insert into TENANT_FIELDS( TENANT_ID, TABLE_NAME, FIELD_NAME, FIELD_TYPE, FIELD_COLUMN ) values ( 'TA', 'LP_TASK', 'DONE', 'VARCHAR(45)', 6 ) ;
+
+-- QUERY
+select T.TABLE_NAME, F.FIELD_NAME, F.FIELD_TYPE, F.FIELD_COLUMN
+from TENANT_TABLE T, TENANT_FIELDS F
+where T.TENANT_ID = F.TENANT_ID
+and T.TENANT_ID = 'TA'
+order by F.FIELD_COLUMN
+
+-- LP_TASK Record Example
+insert into LP_TASK values ( 1001, 'Design Data Models', 'Paul', '2014-09-01', '2014-10-01', 'FALSE' ) ;
+
+
+-- TENANT_DATA Record Example
+insert into TENANT_DATA (RECORD_ID, TENANT_ID, TENANT_TABLE, COLUMN_1, COLUMN_2, COLUMN_3, COLUMN_4, COLUMN_5, COLUMN_6  ) 
+values ( '1001', 'TA', 'LP_TASK', '1001', 'Design Data Models', 'Paul', '2014-09-01', '2014-10-01', 'FALSE' ) ;
+
+-- Test Queries
+select * from LP_TASK ;
+select * from TENANT_DATA where TENANT_ID = 'TA' ;
+
+
+
 -- -----------------------------------------------------
--- Table `c9`.`TENANT_TABLE`
+-- Table `cmpe281`.`TENANT_TABLE`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `c9`.`TENANT_TABLE` (
+CREATE TABLE IF NOT EXISTS `cmpe281`.`TENANT_TABLE` (
   `TENANT_ID` VARCHAR(10) NOT NULL,
   `TABLE_NAME` VARCHAR(45) NOT NULL,
   `TABLE_DESC` VARCHAR(80) NULL,
@@ -11,9 +55,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `c9`.`TENANT_FIELDS`
+-- Table `cmpe281`.`TENANT_FIELDS`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `c9`.`TENANT_FIELDS` (
+CREATE TABLE IF NOT EXISTS `cmpe281`.`TENANT_FIELDS` (
   `TENANT_ID` VARCHAR(10) NOT NULL,
   `TABLE_NAME` VARCHAR(45) NOT NULL,
   `FIELD_NAME` VARCHAR(45) NOT NULL,
@@ -22,16 +66,16 @@ CREATE TABLE IF NOT EXISTS `c9`.`TENANT_FIELDS` (
   PRIMARY KEY (`TENANT_ID`, `TABLE_NAME`, `FIELD_NAME`),
   CONSTRAINT `fk_TENANT_FIELDS_TENANT_TABLE`
     FOREIGN KEY (`TENANT_ID` , `TABLE_NAME`)
-    REFERENCES `c9`.`TENANT_TABLE` (`TENANT_ID` , `TABLE_NAME`)
+    REFERENCES `cmpe281`.`TENANT_TABLE` (`TENANT_ID` , `TABLE_NAME`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `c9`.`TENANT_DATA`
+-- Table `cmpe281`.`TENANT_DATA`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `c9`.`TENANT_DATA` (
+CREATE TABLE IF NOT EXISTS `cmpe281`.`TENANT_DATA` (
   `RECORD_ID` VARCHAR(45) NOT NULL,
   `TENANT_ID` VARCHAR(10) NOT NULL,
   `TENANT_TABLE` VARCHAR(45) NULL,
@@ -76,3 +120,6 @@ values ( '1001', 'TA', 'LP_TASK', '1001', 'Design Data Models', 'Paul', '2014-09
 -- Test Queries
 select * from LP_TASK ;
 select * from TENANT_DATA where TENANT_ID = 'TA' ;
+
+
+
