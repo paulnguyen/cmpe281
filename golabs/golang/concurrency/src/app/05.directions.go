d63a6e4e1dd98e83043207b4b481830e2635bcae
Paul Nguyen <paul.nguyen@sjsu.edu>
2018-04-07 21:47:16 -0700

    Minor change

--- golabs/golang/concurrency/src/app/05.directions.go
+++ golabs/golang/concurrency/src/app/05.directions.go
@@ -17,20 +17,24 @@ import "fmt"
 // values. It would be a compile-time error to try to
 // receive on this channel.
 func ping(pings chan<- string, msg string) {
-    pings <- msg
+	pings <- msg
 }
 
 // The `pong` function accepts one channel for receives
 // (`pings`) and a second for sends (`pongs`).
 func pong(pings <-chan string, pongs chan<- string) {
-    msg := <-pings
-    pongs <- msg
+	msg := <-pings
+	pongs <- msg
 }
 
 func main() {
-    pings := make(chan string, 1)
-    pongs := make(chan string, 1)
-    ping(pings, "passed message")
-    pong(pings, pongs)
-    fmt.Println(<-pongs)
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+
+	go func(pongs <-chan string) {
+		fmt.Println(<-pongs)
+	}(pongs)
+
+	ping(pings, "passed message")
+	pong(pings, pongs)
 }
